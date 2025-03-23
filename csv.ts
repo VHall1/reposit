172507883b9f5e1eb7eb1a2e091152b9947d9d95
vhall1 <victorhallpsn@hotmail.com>
2025-03-23 17:43:24 +0000

    update csv logic use async iterator

--- csv.ts
+++ csv.ts
@@ -1,26 +1,27 @@
 import { parse } from "csv-parse";
 import * as fs from "fs";
 
+// reference: https://csv.js.org/parse/examples/async_iterator/
+
 // Reading the entire file here for the sake of simplicity.
 // Could instead take a callback function to execute logic
 // as the file is being read, if we're expecting CSV files to be exceptionally large.
-export function readFromCSV<T = unknown>(path: string): Promise<Array<T>> {
-  return new Promise<Array<T>>((resolve, reject) => {
-    if (!fs.existsSync(path)) {
-      reject(new Error(`bad CSV path: ${path}`));
-      return;
-    }
+export async function readFromCSV<T = unknown>(
+  path: string
+): Promise<Array<T>> {
+  if (!fs.existsSync(path)) {
+    throw new Error(`bad CSV path, no such file: ${path}`);
+  }
 
-    const data: Array<T> = [];
-    fs.createReadStream(path)
-      .pipe(parse({ columns: true, cast: true }))
-      .on("data", (row) => {
-        // TODO: validate against given type?
-        data.push(row);
-      })
-      .on("end", () => {
-        resolve(data);
-      })
-      .on("error", (err) => reject(err));
-  });
+  const records: Array<T> = [];
+  const parser = fs
+    .createReadStream(path)
+    .pipe(parse({ columns: true, cast: true }));
+
+  for await (const record of parser) {
+    // TODO: validate against given type?
+    records.push(record);
+  }
+
+  return records;
 }
